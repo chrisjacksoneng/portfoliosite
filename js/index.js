3dc858e840b876cc73be84540963ff75cdd4f480
Chris Jackson <2chrisjackson@gmail.com>
2025-02-08 11:14:18 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("home-btn").addEventListener("click", function() {
-        window.location.href = "/index.html"; // Redirects to index.html
+        window.location.href = "/index.html";  // Absolute path
     });
-    
+
     document.getElementById("experience-btn").addEventListener("click", function() {
-        window.location.href = "../pages/experience.html"; // Redirects to experience.html
+        window.location.href = "/pages/experience.html";  // Absolute path
     });
-    
+
     document.getElementById("projects-btn").addEventListener("click", function() {
-        window.location.href = "../pages/projects.html"; // Redirects to projects.html
+        window.location.href = "/pages/projects.html";  // Absolute path
     });
-    
+
     document.getElementById("blogs-btn").addEventListener("click", function() {
-        window.location.href = "../pages/blogs.html"; // Redirects to blogs.html
+        window.location.href = "/pages/blogs.html";  // Absolute path
     });
-    
+
     document.getElementById("resume-btn").addEventListener("click", function() {
-        window.location.href = "../pages/resume.html"; // Redirects to resume.html
+        window.location.href = "/pages/resume.html";  // Absolute path
     });
-    
 });
+
